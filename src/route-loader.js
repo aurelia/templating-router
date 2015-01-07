76389e776e3db94dc916c6f878259b10a4c4b46b
Rob Eisenberg <rob@bluespire.com>
2015-01-07 03:12:36 -0500

    feat(router-view): delayed view loading

--- src/route-loader.js
+++ src/route-loader.js
@@ -1,13 +1,24 @@
-import {ResourceCoordinator} from 'aurelia-templating';
-import {RouteLoader} from 'aurelia-router';
+import {CompositionEngine} from 'aurelia-templating';
+import {RouteLoader, Router} from 'aurelia-router';
 
 export class TemplatingRouteLoader extends RouteLoader {
-  static inject(){ return [ResourceCoordinator]; }
-  constructor(resourceCoordinator){
-    this.resourceCoordinator = resourceCoordinator;
+  static inject(){ return [CompositionEngine]; }
+  constructor(compositionEngine){
+    this.compositionEngine = compositionEngine;
   }
 
-  loadRoute(config){
-    return this.resourceCoordinator.loadViewModelInfo(config.moduleId);
+  loadRoute(router, config){
+    var childContainer = router.container.createChild(),
+        instruction = { viewModel:config.moduleId, childContainer:childContainer },
+        childRouter;
+
+    childContainer.registerHandler(Router, c => { 
+      return childRouter || (childRouter = router.createChild(childContainer)); 
+    });
+
+    return this.compositionEngine.createViewModel(instruction).then(instruction => {
+      instruction.executionContext = instruction.viewModel;
+      return instruction;
+    });
   }
 }

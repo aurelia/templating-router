e6b94ce6f3e9ebdb1e3e1b625225d06fa0a23d82
Bryan Smith <BryanRSmith@gmail.com>
2015-04-18 17:52:35 -0700

    feat(route-href): add custom attribute for generating hrefs for named routes
    fixes aurelia/router#4

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import {Router, AppRouter, RouteLoader} from 'aurelia-router';
 import {TemplatingRouteLoader} from './route-loader';
 import {RouterView} from './router-view';
+import {RouteHref} from './route-href';
 
 function install(aurelia){
   aurelia.withSingleton(RouteLoader, TemplatingRouteLoader)
          .withSingleton(Router, AppRouter)
-         .globalizeResources('./router-view');
+         .globalizeResources('./router-view', './route-href');
 }
 
 export {
   TemplatingRouteLoader,
   RouterView,
+  RouteHref,
   install
 };

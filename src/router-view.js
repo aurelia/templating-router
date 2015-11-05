77db54d12325dd83bc0ed1e50b3da28f3f6794c3
Rob Eisenberg <rob@bluespire.com>
2015-11-05 03:35:09 -0500

    fix(router-view): update to use view locator

--- src/router-view.js
+++ src/router-view.js
@@ -1,18 +1,19 @@
 import {Container, inject} from 'aurelia-dependency-injection';
-import {ViewSlot, ViewStrategy, customElement, noView, BehaviorInstruction} from 'aurelia-templating';
+import {ViewSlot, ViewLocator, customElement, noView, BehaviorInstruction} from 'aurelia-templating';
 import {Router} from 'aurelia-router';
 import {Origin} from 'aurelia-metadata';
 import {DOM} from 'aurelia-pal';
 
 @customElement('router-view')
 @noView
-@inject(DOM.Element, Container, ViewSlot, Router)
+@inject(DOM.Element, Container, ViewSlot, Router, ViewLocator)
 export class RouterView {
-  constructor(element, container, viewSlot, router) {
+  constructor(element, container, viewSlot, router, viewLocator) {
     this.element = element;
     this.container = container;
     this.viewSlot = viewSlot;
     this.router = router;
+    this.viewLocator = viewLocator;
     this.router.registerViewPort(this, this.element.getAttribute('name'));
   }
 
@@ -22,18 +23,13 @@ export class RouterView {
 
   process(viewPortInstruction, waitToSwap) {
     let component = viewPortInstruction.component;
-    let viewStrategy = component.view;
     let childContainer = component.childContainer;
     let viewModel = component.viewModel;
     let viewModelResource = component.viewModelResource;
     let metadata = viewModelResource.metadata;
 
-    if (!viewStrategy && 'getViewStrategy' in viewModel) {
-      viewStrategy = viewModel.getViewStrategy();
-    }
-
+    let viewStrategy = this.viewLocator.getViewStrategy(component.view || viewModel);
     if (viewStrategy) {
-      viewStrategy = ViewStrategy.normalize(viewStrategy);
       viewStrategy.makeRelativeTo(Origin.get(component.router.container.viewModel.constructor).moduleId);
     }
 

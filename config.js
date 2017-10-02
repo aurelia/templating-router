8c96b5f4569c1579d4d5f8956ccd4db1a6561906
EisenbergEffect <rob@bluespire.com>
2017-10-02 00:01:47 -0700

    chore(all): update dependencies

--- config.js
+++ config.js
@@ -131,9 +131,6 @@ System.config({
       "aurelia-path": "npm:aurelia-path@1.1.1",
       "aurelia-route-recognizer": "npm:aurelia-route-recognizer@1.0.0"
     },
-    "npm:aurelia-task-queue@1.2.0": {
-      "aurelia-pal": "npm:aurelia-pal@1.4.0"
-    },
     "npm:aurelia-task-queue@1.2.1": {
       "aurelia-pal": "npm:aurelia-pal@1.4.0"
     },
@@ -161,16 +158,6 @@ System.config({
       "aurelia-path": "npm:aurelia-path@1.1.1",
       "aurelia-router": "npm:aurelia-router@1.0.1",
       "aurelia-templating": "npm:aurelia-templating@1.5.0"
-    },
-    "npm:aurelia-templating@1.3.0": {
-      "aurelia-binding": "npm:aurelia-binding@1.3.0",
-      "aurelia-dependency-injection": "npm:aurelia-dependency-injection@1.3.2",
-      "aurelia-loader": "npm:aurelia-loader@1.0.0",
-      "aurelia-logging": "npm:aurelia-logging@1.3.1",
-      "aurelia-metadata": "npm:aurelia-metadata@1.0.3",
-      "aurelia-pal": "npm:aurelia-pal@1.4.0",
-      "aurelia-path": "npm:aurelia-path@1.1.1",
-      "aurelia-task-queue": "npm:aurelia-task-queue@1.2.1"
     },
     "npm:aurelia-templating@1.5.0": {
       "aurelia-binding": "npm:aurelia-binding@1.3.0",
